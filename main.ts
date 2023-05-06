fcdd8d7db8564c427a667130577ad84db39d67eb
Egyed József <jegyed50@gmail.com>
2023-05-06 17:46:03 +0200

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -4,8 +4,43 @@ led.setBrightness(25)
 music.setVolume(255)
 let x = 2
 let y = 2
+let egg = game.createSprite(x, y)
 basic.forever(function () {
-    led.plot(x, y)
+    egg.set(LedSpriteProperty.X, x)
+    egg.set(LedSpriteProperty.Y, y)
+    if (egg.isTouchingEdge()) {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        880,
+        1760,
+        255,
+        255,
+        200,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        1760,
+        880,
+        255,
+        255,
+        200,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.InBackground)
+    } else {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        1760,
+        880,
+        255,
+        255,
+        100,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.InBackground)
+    }
     accX = input.acceleration(Dimension.X)
     accY = input.acceleration(Dimension.Y)
     if (accX < -150 && x > 0) {
